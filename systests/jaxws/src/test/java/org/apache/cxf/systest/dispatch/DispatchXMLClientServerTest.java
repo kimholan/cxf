10783ad7a54245d97264144816124090f6cb124c
Bhun <bhun.kho@ictu.nl>
2020-09-12 16:09:05 +0200

    messagefactory accepts malformed xml

--- systests/jaxws/src/test/java/org/apache/cxf/systest/dispatch/DispatchXMLClientServerTest.java
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/dispatch/DispatchXMLClientServerTest.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,76 +19,40 @@
 
 package org.apache.cxf.systest.dispatch;
 
-import java.io.InputStream;
-import java.io.StringReader;
-import java.net.URL;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.namespace.QName;
-import javax.xml.transform.Source;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.ws.BindingProvider;
-import javax.xml.ws.Dispatch;
-import javax.xml.ws.Service;
-import javax.xml.ws.WebServiceException;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-
 import org.apache.cxf.staxutils.StaxUtils;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
 import org.apache.cxf.testutil.common.TestUtil;
-import org.apache.hello_world_xml_http.wrapped.XMLService;
-import org.apache.hello_world_xml_http.wrapped.types.GreetMe;
-import org.apache.hello_world_xml_http.wrapped.types.GreetMeResponse;
-import org.apache.hello_world_xml_http.wrapped.types.ObjectFactory;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.w3c.dom.Document;
+
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+import java.io.InputStream;
+import java.io.StringReader;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-public class DispatchXMLClientServerTest extends AbstractBusClientServerTestBase {
+public class DispatchXMLClientServerTest
+        extends AbstractBusClientServerTestBase {
+
     private static final QName SERVICE_NAME
-        = new QName("http://apache.org/hello_world_xml_http/wrapped", "XMLService");
-    private static final QName PORT_NAME
-        = new QName("http://apache.org/hello_world_xml_http/wrapped", "XMLDispatchPort");
+            = new QName("http://apache.org/hello_world_xml_http/wrapped", "XMLService");
 
-    private static String port = TestUtil.getPortNumber(DispatchXMLClientServerTest.class);
+    private static final QName PORT_NAME
+            = new QName("http://apache.org/hello_world_xml_http/wrapped", "XMLDispatchPort");
+
+    private static final String port = TestUtil.getPortNumber(DispatchXMLClientServerTest.class);
+
     @BeforeClass
     public static void startServers() throws Exception {
         createStaticBus();
         assertTrue("server did not launch correctly", launchServer(Server.class, true));
-    }
-
-    @Test
-    public void testJAXBMESSAGE() throws Exception {
-        Service service = Service.create(SERVICE_NAME);
-        assertNotNull(service);
-        service.addPort(PORT_NAME, "http://cxf.apache.org/bindings/xformat",
-                        "http://localhost:"
-                        + port
-                        + "/XMLService/XMLDispatchPort");
-
-
-        GreetMe gm = new GreetMe();
-        gm.setRequestType("CXF");
-        JAXBContext ctx = JAXBContext.newInstance(ObjectFactory.class);
-        Dispatch<Object> disp = service.createDispatch(PORT_NAME, ctx, Service.Mode.MESSAGE);
-        GreetMeResponse resp = (GreetMeResponse)disp.invoke(gm);
-        assertNotNull(resp);
-        assertEquals("Hello CXF", resp.getResponseType());
-
-        try {
-            disp.invoke(null);
-            fail("Should have thrown a fault");
-        } catch (WebServiceException ex) {
-            //expected
-        }
     }
 
     @Test
@@ -101,7 +65,7 @@ public class DispatchXMLClientServerTest extends AbstractBusClientServerTestBase
         Service service = Service.create(SERVICE_NAME);
         assertNotNull(service);
         service.addPort(PORT_NAME, "http://cxf.apache.org/bindings/xformat",
-                        "http://localhost:"
+                "http://localhost:"
                         + port
                         + "/XMLService/XMLDispatchPort");
 
@@ -119,30 +83,4 @@ public class DispatchXMLClientServerTest extends AbstractBusClientServerTestBase
         assertEquals("Hello tli", doc.getFirstChild().getTextContent());
     }
 
-    @Test
-    public void testDOMSourcePAYLOAD() throws Exception {
-        URL wsdl = getClass().getResource("/wsdl/hello_world_xml_wrapped.wsdl");
-        assertNotNull(wsdl);
-
-        XMLService service = new XMLService(wsdl, SERVICE_NAME);
-        assertNotNull(service);
-
-        InputStream is = getClass().getResourceAsStream("/messages/XML_GreetMeDocLiteralReq.xml");
-        Document doc = StaxUtils.read(is);
-        DOMSource reqMsg = new DOMSource(doc);
-        assertNotNull(reqMsg);
-
-        Dispatch<DOMSource> disp = service.createDispatch(PORT_NAME, DOMSource.class,
-                                                          Service.Mode.PAYLOAD);
-        disp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-                                     "http://localhost:"
-                                     + port
-                                     + "/XMLService/XMLDispatchPort");
-        DOMSource result = disp.invoke(reqMsg);
-        assertNotNull(result);
-
-        Node respDoc = result.getNode();
-        assertEquals("greetMeResponse", respDoc.getFirstChild().getLocalName());
-        assertEquals("Hello tli", respDoc.getFirstChild().getTextContent());
-    }
 }
